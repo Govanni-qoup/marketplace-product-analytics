6c4d00b567bb5eabc737d534ee95f3ad8be93a95
Govanni-qoup <giftyluky@gmail.com>
2026-07-02 13:33:51 +0300

    Delete sql/03_arpu_arppu.sql

--- sql/03_arpu_arppu.sql
+++
@@ -1,6 +0,0 @@
-select (select count(distinct user_id) from users) as total_users,
-count(distinct o.user_id) as paying_users,
-sum(o.amount) as revenue,
-sum(o.amount) / (select count(distinct user_id) from users) as arpu,
-sum(o.amount) / count(distinct o.user_id) as arppu
-from orders o
