d87670924a3917f7688e4102af44479f39dcfae3
Govanni-qoup <giftyluky@gmail.com>
2026-07-02 12:31:29 +0300

    Update 03_arpu_arppu.sql

--- sql/03_arpu_arppu.sql
+++ sql/03_arpu_arppu.sql
@@ -1,1 +1,6 @@
-
+select (select count(distinct user_id) from users) as total_users,
+count(distinct o.user_id) as paying_users,
+sum(o.amount) as revenue,
+sum(o.amount) / (select count(distinct user_id) from users) as arpu,
+sum(o.amount) / count(distinct o.user_id) as arppu
+from orders o
